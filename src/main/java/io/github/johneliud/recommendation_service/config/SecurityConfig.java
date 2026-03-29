beb51cf671a4b05ed76c4dec2603bd07446974eb
John <johneliud4@gmail.com>
2026-03-29 18:05:00 +0300

    feat(recommendation-service): add health check endpoint and allow public access.

--- src/main/java/io/github/johneliud/recommendation_service/config/SecurityConfig.java
+++ src/main/java/io/github/johneliud/recommendation_service/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import io.github.johneliud.recommendation_service.security.JwtAuthenticationFilt
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -24,6 +25,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/api/health").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
